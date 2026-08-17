52a986dff3678a3587ea28594855de27a503e925
Lucas Magnun <lucasmagnun1997@gmail.com>
2026-08-17 22:27:31 +0000

    feat: adiciona nestjs config e ajusta prisma service para usar configservice

--- apps/transactions/src/prisma/prisma.service.ts
+++ apps/transactions/src/prisma/prisma.service.ts
@@ -1,12 +1,13 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
+  constructor(configService: ConfigService) {
     const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL,
+      connectionString: configService.get<string>('DATABASE_URL'),
     });
 
     super({ adapter });
